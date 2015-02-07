bb176e0cf20b1af8ab85ba6fc87ee769ee08d8fd
jonathan <jonathan@kyuss.org>
2015-02-07 02:38:21 -0800

    support pre_bundle.sh in MultiBundler

--- appstore-common/src/main/java/cloudos/appstore/bundler/MultiBundlerMain.java
+++ appstore-common/src/main/java/cloudos/appstore/bundler/MultiBundlerMain.java
@@ -5,21 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.exec.CommandLine;
 import org.apache.commons.io.FileUtils;
 import org.cobbzilla.util.io.FileUtil;
 import org.cobbzilla.util.json.JsonUtil;
 import org.cobbzilla.util.system.CommandShell;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+
+import static cloudos.appstore.model.app.AppManifest.CLOUDOS_MANIFEST_JSON;
 
 @Slf4j @AllArgsConstructor
 public class MultiBundlerMain {
 
     public static final String TARGET_DIR = "target";
+    public static final String PRE_BUNDLE_SH = "pre_bundle.sh";
     @Getter @Setter private String[] args;
 
     private final Map<String, Object> results = new HashMap<>();
@@ -53,24 +54,46 @@ public class MultiBundlerMain {
     }
 
     private void processDir(File dir) throws Exception {
-        for (File f : dir.listFiles()) {
+
+        final Set<String> processed = new HashSet<>();
+        final File[] files = dir.listFiles();
+        if (files == null) throw new IllegalArgumentException("Invalid directory: "+dir.getAbsolutePath());
+
+        for (File f : files) {
+            final String fpath = f.getAbsolutePath();
+
             if (f.isDirectory()) {
                 if (f.getName().equals(TARGET_DIR)) continue;
                 processDir(f);
             }
-            if (f.isFile() && f.getName().equals("cloudos-manifest.json")) {
+
+            if (!processed.contains(fpath)
+                    && f.isFile()
+                    && (f.getName().equals(CLOUDOS_MANIFEST_JSON) || f.getName().equals(PRE_BUNDLE_SH))) {
                 final String appName = f.getParentFile().getName();
                 try {
                     bundleApp(f);
                     results.put(appName, "success!");
                 } catch (Exception e) {
                     results.put(appName, e);
+                } finally {
+                    processed.add(fpath);
                 }
             }
         }
     }
 
     private void bundleApp(File manifestFile) throws Exception {
+
+        // If the "manifest" is actually a pre_bundle.sh script, run it, then look for the manifest
+        if (manifestFile.getName().equals(PRE_BUNDLE_SH)) {
+            CommandShell.exec(new CommandLine(manifestFile));
+            final File realManifest = new File(manifestFile.getParentFile(), CLOUDOS_MANIFEST_JSON);
+            if (!realManifest.exists()) {
+                throw new IllegalStateException(CLOUDOS_MANIFEST_JSON+" does not exist, even after running: "+manifestFile.getAbsolutePath());
+            }
+            manifestFile = realManifest;
+        }
 
         final AppManifest manifest = JsonUtil.fromJson(FileUtil.toString(manifestFile), AppManifest.class);
         final String appName = manifestFile.getParentFile().getName();
