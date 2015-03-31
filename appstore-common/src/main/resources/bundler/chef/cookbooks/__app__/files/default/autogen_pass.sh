215ebb6c29a1a6f00a229808868fc80124a7e604
jonathan <jonathan@kyuss.org>
2015-03-30 19:48:57 -0700

    add logging

--- appstore-common/src/main/resources/bundler/chef/cookbooks/__app__/files/default/autogen_pass.sh
+++ appstore-common/src/main/resources/bundler/chef/cookbooks/__app__/files/default/autogen_pass.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "autogen_pass called as: $0 $@"
+#LOG=/tmp/$(basename $0).$(date +%Y%m%d-%H%M%S).$$.log
+LOG=/dev/null
+
+echo "autogen_pass called as: $0 $@" | tee -a ${LOG}
 
 JSON_EDITOR="${1}"
 APP_NAME="${2}"
@@ -12,42 +15,42 @@ ADMIN_EMAIL="${7}"
 LENGTH=${8}
 
 if [ -z "${JSON_EDITOR}" ] ; then
-  echo "No JSON_EDITOR specified as first argument"
+  echo "No JSON_EDITOR specified as first argument" | tee -a ${LOG}
   exit 2
 fi
 
 # if this is a brand new cloudstead, we might not have sendmail yet
 if [ -z "$(which sendmail)" ] ; then
-  echo "No sendmail found, installing postfix..."
+  echo "No sendmail found, installing postfix..." | tee -a ${LOG}
   apt-get install -y postfix
   echo "postfix installed"
 fi
 
 # check that password is not already set
-current_pass=$(cat ${DATABAG_FILE} | ${JSON_EDITOR} -p "${JSON_PATH}" | tr -d ' ')
+current_pass="$(cat ${DATABAG_FILE} | ${JSON_EDITOR} -p ${JSON_PATH} | tr -d ' ')"
 if [ ! -z "${current_pass}" ] ; then
-  echo "Password already generated, exiting"
+  echo "Password already generated, exiting" | tee -a ${LOG}
   exit 0
 fi
 
 new_password="$(tr -dc A-Za-z0-0_ < /dev/urandom | head -c ${LENGTH})"
 if [ -z "${new_password}" ] ; then
-  echo "Empty password generated!"
+  echo "Empty password generated!" | tee -a ${LOG}
   exit 2
 fi
 
 backup=$(mktemp /tmp/json.XXXXXX)
 
 cat ${DATABAG_FILE} > ${backup}
-echo "Generated password, updating ${DATABAG_FILE} (path=${JSON_PATH})"
+echo "Generated password, updating ${DATABAG_FILE} (path=${JSON_PATH})" | tee -a ${LOG}
 
-cat ${DATABAG_FILE} | ${JSON_EDITOR} -p "${JSON_PATH}" -v ${new_password} -o ${DATABAG_FILE}
+cat ${DATABAG_FILE} | ${JSON_EDITOR} -p ${JSON_PATH} -v "${new_password}" -o ${DATABAG_FILE}
 if [ $? -ne 0 ] ; then
-  echo "Error updating databag ${DATABAG_FILE} with autogen password"
+  echo "Error updating databag ${DATABAG_FILE} with autogen password" | tee -a ${LOG}
   cp ${backup} ${DATABAG_FILE}
   exit 1
 fi
-echo "Databag updated, sending email"
+echo "Databag updated, sending email" | tee -a ${LOG}
 
 sendmail -oi -t 2> /tmp/autogen_$(date +%s) <<EOMAIL
 From: do-not-reply@$(hostname)
@@ -70,11 +73,11 @@ EOMAIL
 rval=$?
 
 if [ ${rval} -ne 0 ] ; then
-    echo "Failed to send email, rolling back databag ${DATABAG_FILE}"
+    echo "Failed to send email, rolling back databag ${DATABAG_FILE}" | tee -a ${LOG}
     cp ${backup} ${DATABAG_FILE}
     exit 1
 fi
 
-echo "Email sent successfully, removing backup databag file"
+echo "Email sent successfully, removing backup databag file" | tee -a ${LOG}
 rm ${backup}
 exit 0
