181f9cc783dfe75815b28066c9a3a1db2a2bcfc7
jonathan <jonathan@kyuss.org>
2015-04-29 02:25:42 -0700

    support backing up multiple databases

--- appstore-common/src/main/java/cloudos/appstore/model/app/AppBackupConfig.java
+++ appstore-common/src/main/java/cloudos/appstore/model/app/AppBackupConfig.java
@@ -6,7 +6,8 @@ import lombok.Setter;
 import java.util.List;
 
 public class AppBackupConfig {
-    @Getter @Setter private List<String> files;
     @Getter @Setter private AppShellCommand[] pre;
+    @Getter @Setter private List<String> files;
+    @Getter @Setter private List<String> databases;
     @Getter @Setter private AppShellCommand[] post;
 }
