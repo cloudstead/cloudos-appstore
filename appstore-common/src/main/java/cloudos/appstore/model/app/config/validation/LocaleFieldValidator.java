ef06ee7831ffec743b41feed486d3864b39197cf
jonathan <jonathan@kyuss.org>
2015-05-29 12:37:22 -0700

    add note about better validation

--- appstore-common/src/main/java/cloudos/appstore/model/app/config/validation/LocaleFieldValidator.java
+++ appstore-common/src/main/java/cloudos/appstore/model/app/config/validation/LocaleFieldValidator.java
@@ -7,6 +7,7 @@ import static org.cobbzilla.wizard.validation.ValidationRegexes.LOCALE_PATTERNS;
 public class LocaleFieldValidator extends PickOneFieldValidator {
 
     @Override protected boolean isValid(String value) {
+        // todo: we should load default-locale-names.json and validate that the value exactly matches something there
         for (Pattern p : LOCALE_PATTERNS) {
             if (p.matcher(value).matches()) return true;
         }
