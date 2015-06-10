8f31d7954c3a6f64e703d7638302a7397fc08d0c
jonathan <jonathan@kyuss.org>
2015-06-10 08:52:04 -0700

    @lang is a valid value for locale

--- appstore-common/src/main/java/cloudos/appstore/model/app/config/validation/LocaleFieldValidator.java
+++ appstore-common/src/main/java/cloudos/appstore/model/app/config/validation/LocaleFieldValidator.java
@@ -7,6 +7,10 @@ import static org.cobbzilla.wizard.validation.ValidationRegexes.LOCALE_PATTERNS;
 public class LocaleFieldValidator extends PickOneFieldValidator {
 
     @Override protected boolean isValid(String value) {
+
+        // allow value of "@lang", chef installer will substitute at install-time
+        if ("@lang".equals(value)) return true;
+
         // todo: we should load default-locale-names.json and validate that the value exactly matches something there
         for (Pattern p : LOCALE_PATTERNS) {
             if (p.matcher(value).matches()) return true;
