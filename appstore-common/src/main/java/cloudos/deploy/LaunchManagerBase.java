6d1a6b5cb59de2313646915dd6a335cce28b4e3a
jonathan <jonathan@kyuss.org>
2015-08-29 12:22:39 -0700

    remove redundancies from refactoring

--- appstore-common/src/main/java/cloudos/deploy/LaunchManagerBase.java
+++ appstore-common/src/main/java/cloudos/deploy/LaunchManagerBase.java
@@ -3,16 +3,13 @@ package cloudos.deploy;
 import cloudos.model.instance.CloudOsBase;
 import cloudos.model.instance.CloudOsState;
 import cloudos.model.instance.CloudOsTaskResultBase;
-import edu.emory.mathcs.backport.java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.map.LRUMap;
 import org.cobbzilla.util.system.Sleep;
 import org.cobbzilla.wizard.model.UniquelyNamedEntity;
 import org.cobbzilla.wizard.server.config.RestServerConfiguration;
 import org.cobbzilla.wizard.task.TaskId;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.die;
@@ -27,8 +24,6 @@ public abstract class LaunchManagerBase<A extends UniquelyNamedEntity,
 
     @Autowired protected CloudOsTaskService<A, C, T, R> taskService;
 
-    protected final Map<String, T> launchers = Collections.synchronizedMap(new LRUMap(100));
-
     public abstract RestServerConfiguration getConfiguration ();
 
     protected T launchTask(A account, C instance) {
@@ -40,24 +35,14 @@ public abstract class LaunchManagerBase<A extends UniquelyNamedEntity,
 
     public R getResult(TaskId taskId) { return taskService.getResult(taskId.getUuid()); }
 
-    public boolean isRunning(C cloudOs) { return taskService.isRunning(cloudOs.getUuid()); }
+    public boolean isRunning(C instance) { return taskService.isRunning(instance.getUuid()); }
 
     public TaskId launch(A account, C instance) {
         if (!instance.getAdminUuid().equals(account.getUuid())) die("launch: not owner");
 
-        final T task = launchTask(account, instance);
-        final T existing = launchers.get(instance.getUuid());
-        if (existing != null) {
-            if (!existing.getResult().isComplete()) {
-                die("launch: already launching with taskId: " + existing.getResult());
-            } else if (existing.getResult().getCloudOs().isRunning()) {
-                die("launch: already running: "+existing.getResult());
-            } else if (!existing.getResult().getCloudOs().isLaunchable()) {
-                die("launch: not launchable: "+existing.getResult());
-            }
-        }
+        if (isRunning(instance)) die("launch: already running");
 
-        launchers.put(instance.getUuid(), task);
+        final T task = launchTask(account, instance);
         return taskService.execute(task);
     }
 
@@ -71,7 +56,7 @@ public abstract class LaunchManagerBase<A extends UniquelyNamedEntity,
             return instance.getState();
         }
 
-        T task = launchers.remove(instance.getUuid());
+        T task = taskService.getTask(instance.getUuid());
         if (task != null) task = taskService.cancel(task.getTaskId().getUuid());
 
         if (task == null) {
